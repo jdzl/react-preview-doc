1b3b3af9599c501cf77e22baf3461c65b5b7fa40
David <00jdzl@gmail.com>
2021-05-15 18:05:33 -0500

    Updates

---
+++ precache-manifest.28094b30af839562b0c519c14d337d26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61b99e442da84ab460fa994c5d6654f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "54088cb9ba60db2b6789",
+    "url": "./static/css/main.4eb98b61.chunk.css"
+  },
+  {
+    "revision": "14d4bbfa03fa1bdee105",
+    "url": "./static/js/2.26483958.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.26483958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54088cb9ba60db2b6789",
+    "url": "./static/js/main.aad36f15.chunk.js"
+  },
+  {
+    "revision": "620eca207abd02b7a9f9",
+    "url": "./static/js/runtime-main.b7ea5307.js"
+  }
+]);
